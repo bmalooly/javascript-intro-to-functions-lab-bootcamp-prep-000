a4f849feacf6f707c61144016f1a76caf512eea6
Brittany Malooly <bmalooly@gmail.com>
2018-12-31 17:13:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ function logWhisper(string){console.log(string.toLowerCase('HELLO'))}
 function sayHitoGrandma(string) {
 if (string === string.toLowerCase()) return "I can't hear you!"
 else if (string === string.toUpperCase()) return "YES INDEED!"
-else (string === "I love you, Grandma.") return "I love you, too."}
+else if (string === "I love you, Grandma.") return "I love you, too."}
